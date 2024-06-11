2e17c63238a95827ebebcb56709f49cd26ac60d4
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-11 01:06:12 -0700

    Styled 404 page and added back link

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const PORT = process.env.PORT || 3500;
 app.use('/', express.static(path.join(__dirname, '/public')));
 app.use('/', require('./routes/root.route'));
 
+
+
+app.all('*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, '..', 'server', 'public', 'views', '404.html'));
+  }
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
