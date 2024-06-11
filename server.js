44a8168d0c8b638bf52469c2a892458ebd9009d5
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-11 01:09:05 -0700

    added text and json to accepted 404 response types

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.all('*', (req, res) => {
   res.status(404);
   if (req.accepts('html')) {
     res.sendFile(path.join(__dirname, '..', 'server', 'public', 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({message: "404 Not Found"});
+  } else {
+    res.type('text').send("404 Not Found")
   }
 })
 
