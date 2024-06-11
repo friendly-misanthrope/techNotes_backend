b1b1747acbc2b3125d975538970e8d53fed00457
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-11 01:44:14 -0700

    implemented logEvents function

--- middleware/logger.js
+++ middleware/logger.js
@@ -3,3 +3,17 @@ const { v4: uuid } = require('uuid');
 const fsPromises = require('fs').promises;
 const path = require('path');
 
+const logEvents = async (msg, fileName) => {
+  const dateTime = `${format(new Date(), 'yyMMdd\tHH:mm:ss')}`;
+  const logItem = `${dateTime}\t${uuid()}\t${msg}\n`;
+  const logPath = path.join(__dirname, '..', 'logs')
+
+  try {
+    if (!fs.existsSync(path.join(logPath))) {
+      await fsPromises.mkdir(logPath);
+    }
+    await fsPromises.appendFile(path.join(logPath, fileName), logItem);
+  } catch (e) {
+    console.log(e);
+  }
+}
