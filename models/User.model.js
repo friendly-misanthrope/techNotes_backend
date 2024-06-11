97980df820c9e20e0ee2e4a66694bc2c8102cfa8
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-11 07:41:30 -0700

    created User mongoose model

---
+++ models/User.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    minLength: [4, "Username must be at least 4 characters"],
+    maxLength: [16, "Username must be between 4 and 16 characters"]
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minLength: [8, "Password must be at least 8 characters"],
+    maxLength: [64, "Password must be between 8 and 64 characters"]
+  },
+  roles: [{
+    type: String,
+    default: "Employee"
+  }],
+  isActive: {
+    type: Boolean,
+    default: true
+  }
+}, {
+  timestamps: true,
+  toObject: { virutals: true }
+});
+
+module.exports = mongoose.model('User', UserSchema)
