5c4b4d93fb40da6f8cfccb3b4540bc0ebe98f69c
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-13 21:47:09 -0700

    refactored compare password checks to use custom middleware instead of virtuals

--- models/User.model.js
+++ models/User.model.js
@@ -26,13 +26,31 @@ const UserSchema = new mongoose.Schema({
   toObject: { virutals: true }
 });
 
-/* Virtual confirmPassword field */
-UserSchema.virtual('confirmPassword')
-  .set(function (confPass) {
-    this._confirmPassword = confPass
-  })
-  .get(function () {
-    return this._confirmPassword
-  });
+// ToDo
+/* Refactor to compare passwords without using virutals
+create compare password function
+on User schema (Schema.methods.comparePassword(password)),
+& call method on user instance from controller. */
 
-module.exports = mongoose.model('User', UserSchema)
+/* Virtual confirmPassword field */
+// UserSchema.virtual('confirmPassword')
+//   .set(function (confPass) {
+//     this._confirmPassword = confPass
+//   })
+//   .get(function () {
+//     return this._confirmPassword
+//   });
+
+//ToDo: Put this in user registration controller
+UserSchema.methods.comparePasswords = (req, res, next) => {
+  if (!req.password || req.password !== req.confirmPassword) {
+    return res.status(400).json({ message: "Passwords do not match" });
+  }
+  next();
+}
+
+UserSchema.methods.checkLoginPassword = (req, res, next) => {
+
+}
+
+module.exports = mongoose.model('User', UserSchema);
