416380e36c34fa56e7ad396d226e4801dd2a0152
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-13 23:24:40 -0700

    installed asyncHandler dependency, created controller for users

---
+++ controllers/users.controller.js
@@ -0,0 +1,11 @@
+const Users = require('../models/User.model');
+const Notes = require('../models/Note.model');
+const asyncHandler = require('express-async-handler');
+const argon2 = require('argon2');
+
+// Get all users
+const getAllUsers = asyncHandler(async (req, res) => {
+
+})
+
+module.exports = { getAllUsers }
