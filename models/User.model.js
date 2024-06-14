2b7f1c023f366ac228621ce81ce16d6d5211c33d
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-14 01:40:47 -0700

    removed mongoose middleware from model

--- models/User.model.js
+++ models/User.model.js
@@ -22,35 +22,8 @@ const UserSchema = new mongoose.Schema({
     default: true
   }
 }, {
-  timestamps: true,
-  toObject: { virutals: true }
+  timestamps: true
 });
 
-// ToDo
-/* Refactor to compare passwords without using virutals
-create compare password function
-on User schema (Schema.methods.comparePassword(password)),
-& call method on user instance from controller. */
-
-/* Virtual confirmPassword field */
-// UserSchema.virtual('confirmPassword')
-//   .set(function (confPass) {
-//     this._confirmPassword = confPass
-//   })
-//   .get(function () {
-//     return this._confirmPassword
-//   });
-
-//ToDo: Put this in user registration controller
-UserSchema.methods.comparePasswords = (req, res, next) => {
-  if (!req.password || req.password !== req.confirmPassword) {
-    return res.status(400).json({ message: "Passwords do not match" });
-  }
-  next();
-}
-
-UserSchema.methods.checkLoginPassword = (req, res, next) => {
-
-}
 
 module.exports = mongoose.model('User', UserSchema);
