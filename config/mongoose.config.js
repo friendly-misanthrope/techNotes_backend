fb200b8e6f90c6c9d2a69efb6ec9e20d1ba8c281
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-13 23:11:47 -0700

    Added error logging for mongoDB connection

--- config/mongoose.config.js
+++ config/mongoose.config.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+const { logEvents } = require('../middleware/logger')
 
-mongoose.connect('mongodb://127.0.0.1:27017/techNotes')
+mongoose.connect(process.env.DB_CONNECTION)
   .then(() => console.log('Database connection established'))
   .catch(e => console.log('Connection to database failed', e));
+
+mongoose.connection.on('error', (err) => {
+  console.log(err);
+  logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    'mongoErrs.log'
+  )
+});
