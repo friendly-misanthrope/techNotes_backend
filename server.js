2f05d665169b1f262563ab48081891ca49ef6f96
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-14 01:39:16 -0700

    Added express.json() middleware

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(logger);
 
 /* BUILT-IN MIDDLEWARE */
 app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json())
 
 /* THIRD-PARTY MIDDLEWARE */
 app.use(cookieParser());
