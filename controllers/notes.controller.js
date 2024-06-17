7127bc02a4c43f7353c60023373784bca0d40b7d
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-17 01:25:15 -0700

    added logic for creating a note and updating the user's 'notes' array

--- controllers/notes.controller.js
+++ controllers/notes.controller.js
@@ -33,6 +33,47 @@ const getOneNoteWithUser = asyncHandler(async (req, res) => {
 /* CREATE NOTE AND ASSIGN IT A USER */
 const createNote = asyncHandler(async (req, res) => {
 
+  const {
+    assignedUserId,
+    title,
+    content,
+    isCompleted
+  } = req.body;
+
+  const foundUser = await Users.findbyId(assignedUserId).lean().exec();
+
+  if (!foundUser) {
+    return res.status(400).json({
+      message: `User with ID ${assignedUserId} not found`
+    });
+  }
+
+  const newNote = await Notes.create({
+    assignedUser: foundUser._id,
+    title,
+    content,
+    isCompleted
+  });
+
+  if (newNote) {
+    const updatedUser = await Users.findOneAndUpdate(
+      { _id: newNote.assignedUser },
+      { ...foundUser, notes: foundUser.notes.push(newNote) },
+      { new: true }
+    );
+    if (updatedUser) {
+      return res.status(201).json({
+        message: `New note '${newNote.title}' created and assigned to ${updatedUser.username}`
+      })
+    } else {
+      return res.status(400).json({
+        message: `Unable to update user with new note`
+      });
+    }
+  }
+  res.status(400).json({
+    message: `Unable to create new note`
+  });
 });
 
 
