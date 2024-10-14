e72edef123a28f3796aa7edc7a6d2fbb3ef79e87
friendly-misanthrope <nickthedevguy@gmail.com>
2024-10-14 14:05:38 -0700

    changed maxLength of title and minLength of content

--- models/Note.model.js
+++ models/Note.model.js
@@ -10,12 +10,12 @@ const NoteSchema = new mongoose.Schema({
     type: String,
     required: [true, "Note title is required"],
     minLength: [4, "Note title must be at least 4 characters"],
-    maxLength: [16, "Note title must be between 4 and 16 characters"]
+    maxLength: [32, "Note title must be between 4 and 32 characters"]
   },
   content: {
     type: String,
     required: [true, "Note body cannot be blank"],
-    minLength: [16, "Note body must be at least 16 characters"],
+    minLength: [4, "Note body must be at least 4 characters"],
     maxLength: [760, "Note body must be between 16 and 760 characters"]
   },
   isCompleted: {
